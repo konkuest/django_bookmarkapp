6a77587e50d960be3d037be1443e3b5ede930d98
Alex Kyungho Park <kyungho.dev@gmail.com>
2018-01-01 17:43:05 +0900

    #3 Established Model
    model created and registered on admin, migrated.
    
    Checked server running

--- bookmark/mark/admin.py
+++ bookmark/mark/admin.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from mark.models import Bookmark #Added
+
 # Register your models here.
+
+# Added for defining BookmarkAdmin
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url')
+
+# Added for register
+admin.site.register(Bookmark, BookmarkAdmin)
